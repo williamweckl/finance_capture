831a6f7026e37cb7e6850ef3f047ca1a954133fa
William Weckl <william.weckl@gmail.com>
2016-03-10 18:11:41 -0300

    Commodities list on redis.

--- app/controllers/commodities_controller.rb
+++ app/controllers/commodities_controller.rb
@@ -6,7 +6,8 @@ class CommoditiesController < ApplicationController
     sse = SSE.new(response.stream)
     begin
       loop do
-        sse.write('a')
+        $redis.zadd("commodities", 10.0, 'GOOG')
+        sse.write({name: 'GOOG', price_sales: 10.0}.to_json)
         sleep 1
       end
     rescue IOError
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,12 @@
 class PagesController < ApplicationController
   def home
-    @commodities = [{name: 'foo', price_sales: 'bar'}]
+    companies = ["YHOO","AAPL","GOOG","MSFT","BZK16.NYM"]
+    @commodities = []
+    companies.each do |company|
+      price_sales = $redis.zscore('commodities', company)
+      if price_sales
+        @commodities << {name: company, price_sales: price_sales}
+      end
+    end
   end
 end
